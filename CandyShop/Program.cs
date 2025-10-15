03640d35ca7ef37410de8db68da694f3b7200331
AndrewPigeon <andrewpigeon904@gmail.com>
2025-10-15 23:05:49 +0300

    ДЗ 15.10.25

---
+++ CandyShop/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+
+namespace Copywright
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write( "Enter path to file: ");             //Зчітування шляху до файлу ЯКИЙ копіюємо
+            string userPath = $@"{Console.ReadLine()}";
+
+            if (File.Exists(userPath))                          //Перевірка чи існує файл
+            {
+                Console.WriteLine("File exist");
+                //Console.WriteLine(userPath);
+            }
+            else
+            {
+                Console.WriteLine("File does'nt exxist yet");
+                //Console.WriteLine(userPath);
+            }
+
+
+            Console.Write("Enter path to copy: ");              //Зчітування шляху КУДИ копіюємо
+            string destinationPath = $@"{Console.ReadLine()}";
+
+
+            File.Copy(userPath, destinationPath);               //Власне копіювання файлу
+            Console.WriteLine("Файл скопійовано.");
+
+
+            if (File.Exists(destinationPath))                   //Перевірка чи скопіювався файл
+            {
+                Console.WriteLine("File exist");
+                //Console.WriteLine(userPath);
+            }
+            else
+            {
+                Console.WriteLine("File does'nt exxist yet");
+                //Console.WriteLine(userPath);
+            }
+
+
+
+
+
+        }
+
+
+
+
+
+    }
+}
+
+
+
+
